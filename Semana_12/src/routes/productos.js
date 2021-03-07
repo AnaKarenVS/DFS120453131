0eb559528b8a3f0448dedc5d322b6fa5fa35d0c9
unknown <akarenvs@outlook.com>
2021-03-07 12:07:31 -0600

    Crud de productos

--- Semana_12/src/routes/productos.js
+++ Semana_12/src/routes/productos.js
@@ -1,50 +1,50 @@
-const express  = require('express'); //servidor 
+const express = require('express'); //servidor 
 const passport = require('passport');
 const router = express.Router();
 const Producto = require('../model/producto');
 
-router.get('/productos/listaProductos', async (req, res)=>{
-    const productos = await  Producto.find().sort({_id:'asc'}).lean();
+router.get('/productos/listaProductos', async (req, res) => {
+    const productos = await Producto.find().sort({ _id: 'asc' }).lean();
     console.log(productos);
-    res.render('productos/ListaProductos', {productos});
-   
+    res.render('productos/ListaProductos', { productos });
+
 });
 
-router.get('/productos/agregarProductos',(req, res)=>{
+router.get('/productos/agregarProductos', (req, res) => {
     res.render('productos/AgregarProducto');
 });
 
-router.post('/productos/agregarProductos', async (req, res)=>{
-    const { _id, nombreProducto, descripcion, cantidad, precio} = req.body;
+router.post('/productos/agregarProductos', async (req, res) => {
+    const { _id, nombreProducto, descripcion, cantidad, precio } = req.body;
     console.log(req.body);
     const errors = []
-    if(!_id){
+    if (!_id) {
         errors.push({
             text: 'Falta el ID del producto'
         });
     }
-    if(!nombreProducto){
+    if (!nombreProducto) {
         errors.push({
             text: 'Falta el nombre del producto'
         });
     }
-    if(!descripcion){
+    if (!descripcion) {
         errors.push({
             text: 'Falta la descripción del producto'
         });
     }
-    if(!cantidad){
+    if (!cantidad) {
         errors.push({
             text: 'Falta la cantidad del producto'
         });
     }
-    if(!precio){
+    if (!precio) {
         errors.push({
             text: 'Falta el precio del producto'
         });
     }
 
-    if(errors.length > 0){
+    if (errors.length > 0) {
         errors.forEach(element => {
             console.log(element);
             req.flash('errorMessage', element.text);
@@ -52,12 +52,35 @@ router.post('/productos/agregarProductos', async (req, res)=>{
         res.redirect('/productos/listaProductos');
         console.table(errors);
 
-    }else{
-        const newProducto = new Producto({_id, nombreProducto, descripcion, cantidad, precio});
+    } else {
+        const newProducto = new Producto({ _id, nombreProducto, descripcion, cantidad, precio });
         await newProducto.save();
         req.flash('successMessage', 'Producto agregado correctamente');
         res.redirect('/productos/listaProductos');
     }
+
+});
+
+ //update
+ router.put('/productos/editarProducto/:_id', async (req, res) => {
+    const { _id, nombreProducto, descripcion, cantidad, precio } = req.body;
+    await Producto.findByIdAndUpdate(req.params._id, { nombreProducto, descripcion, cantidad, precio });
+    console.log("llego");
+    req.flash('successMessage', 'Registro actualizado');
+    res.redirect('/productos/listaProductos');
+});
+
+router.get('/productos/editarProducto/:_id', async (req, res) => {
+    const updateProducto = await Producto.findById(req.params._id).lean();
+
+    console.log("producto a actualizar", updateProducto);
+    res.render('productos/EditarProducto', { updateProducto });
+});
+
+router.get('/productos/eliminarProducto/:_id', async (req, res) => {
+    await Producto.findByIdAndRemove(req.params._id);
+    req.flash('successMessage', 'Producto eliminado correctamente');
+    res.redirect('/productos/listaProductos');
 
 });
 
